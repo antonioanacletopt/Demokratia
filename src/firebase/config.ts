7e9d819ee6995a394ad838df214c36fdb7b77a2f
António Anacleto <antonio.anacleto@gmail.com>
2026-02-13 17:29:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1716481110-b0153",
+  "appId": "1:1032861335964:web:3aed306f3a909b517e0859",
+  "apiKey": "AIzaSyC_qSyHNiwAOfPFrV5kZQ5wQAOQYz351DY",
+  "authDomain": "studio-1716481110-b0153.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1032861335964"
+};
