d2d14565a26b7d6d93c120aa39d42a55b46d4365
António Anacleto <antonio.anacleto@gmail.com>
2026-02-14 12:05:47 +0000

    0 botao de menu nao funciona

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,19 +1,31 @@
+"use client"
+
 import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState(false)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    const mediaQuery = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+
+    // Set the initial state from the media query
+    setIsMobile(mediaQuery.matches)
+
+    // Create a listener for changes
+    const handleResize = (event: MediaQueryListEvent) => {
+      setIsMobile(event.matches)
     }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+
+    // Add the listener
+    mediaQuery.addEventListener("change", handleResize)
+
+    // Clean up the listener on component unmount
+    return () => {
+      mediaQuery.removeEventListener("change", handleResize)
+    }
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
